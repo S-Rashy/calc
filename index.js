12a2a3dbf63ea9108938e5db1d6d6f3075d17030
S-Rashy <sadiqrashidah@gmail.com>
2024-09-12 03:42:26 -0700

    another commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 let showtext = document.getElementById("display");
 
+
 function addToDisplay(btn) {
+    if (showtext.textContent.length >= 20) {
+        alert("Equation too long")
+    }
+    
     showtext.textContent += btn
+
 }
 
 function equal() { 
-    showtext.textContent = eval(showtext.textContent);    
+//     let lastnum = showtext.textContent.slice( 0,-1)
+//     // let firstnum = showtext.teazxtContent.charAt[0]
+
+//   if ( lastnum === '+'|| lastnum == '-' || lastnum == '*' || lastnum == '/' ) {
+//     showtext.textContent = "Equation Invalid"
+// } 
+    // else if ( firstnum ==  '*' || firstnum == '/'  ) {
+    //     showtext.textContent = "Equation Invalid"
+    // }
+    
+    showtext.textContent = eval(showtext.textContent);       
+    
 }
 
 function clearDisplay() { 
@@ -15,4 +32,17 @@ function clearDisplay() {
 function deleteNum() { 
     showtext.textContent = showtext.textContent.slice(0, -1);    
 }
+function toPercent() {
+    showtext.textContent = showtext.textContent/100
+
+}
+function square() {
+    showtext.textContent = showtext.textContent**2
+}
+
+function squareroot() {
+    showtext.textContent = Math.sqrt(showtext.textContent);
+}
+
+
 
